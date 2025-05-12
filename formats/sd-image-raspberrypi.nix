65c4dcbd09e1bef38ca737948729a3bb9fe37cb5
Logan Barnett <logustus@gmail.com>
2025-05-11 20:01:21 -0700

    fixup! add Raspberry Pi image

--- formats/sd-image-raspberrypi.nix
+++ formats/sd-image-raspberrypi.nix
@@ -3,5 +3,7 @@ in {
   imports = [
     "${toString modulesPath}/installer/sd-card/sd-image-raspberrypi.nix"
   ];
+  # This formatAttr must be from some fixed list.  Where and why?  This could go
+  # in the documentation.  I don't think it's documented presently.
   formatAttr = "sdImage";
 }
